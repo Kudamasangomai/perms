5106d6b764132627f34157e30f565705d4f28c9a
Kuda <kudam775@gmail.com>
2022-08-17 14:00:57 +0200

    admin name changing

--- perms/urls.py
+++ perms/urls.py
@@ -13,12 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from re import template
+
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path,include
 from django.contrib.auth import views as auth_views
+
+admin.site.site_header = "Plant Quarantine Services Admin Portal"
+admin.site.site_title = "Plant Quarantine Services Admin Portal"
+admin.site.index_title = "Admin"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
